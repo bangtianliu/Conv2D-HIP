fa1e8971e99b9b33be7daa0d8bb8dae6e1d7e068
Bangtian Liu <bangtian.liu@amd.com>
2024-04-23 12:02:56 -0700

    One more general implemenattion

--- conv_config.hpp
+++ conv_config.hpp
@@ -2,13 +2,13 @@
 #define CONV_CONFIG_HPP
 
 #define CONV_N 1
-#define CONV_IN_C 3 
-#define CONV_IN_H 1920 
-#define CONV_IN_W 2400
+#define CONV_IN_C 1280 
+#define CONV_IN_H 32
+#define CONV_IN_W 32
 
-#define CONV_F_H 6 
-#define CONV_F_W 6
-#define CONV_OUT_C 3
+#define CONV_F_H 3
+#define CONV_F_W 3
+#define CONV_OUT_C 1280
 
 #define CONV_OUT_H (CONV_IN_H - CONV_F_H + 1)
 #define CONV_OUT_W (CONV_IN_W - CONV_F_W + 1)
